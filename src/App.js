9521b7a7a3299144c3aecf12e86f059b643217af
Kouame N'Dri <56895441+kndri@users.noreply.github.com>
2021-01-14 02:44:02 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* Home */}
+    </div>
+  );
+}
